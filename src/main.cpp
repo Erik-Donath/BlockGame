3fc872bbfe0e22f8be4c320a52bedf6521ad2175
C42xe2 <erikdonath2006@gmail.com>
2024-04-21 21:57:31 +0200

    Update main.cpp
    Die Buffer existieren jetzt in einem Objekt.

--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,44 @@
 const char* vs_src = "#version 330 core\nlayout (location = 0) in vec4 aPos;\nout vec4 vPos;\nvoid main() {\n    vPos = aPos;\n    gl_Position = aPos;\n}\0";
 const char* fs_src = "#version 330 core\nin vec4 vPos;\nout vec4 FragColor;\nvoid main() {\n    FragColor = vec4((vPos.r + 1) / 2, (vPos.g + 1) / 2, 0.0f, 1.0f);\n}\0";
 
-float vertices[] = {
+float obj_vertices[] = {
         0.5f, 0.5f,   // top right
         0.5f, -0.5f,  // bottom right
         -0.5f,-0.5f,  // bottom left
         -0.5f,0.5f    // top left
 };
-GLubyte indices[] = {     // note that we start from 0
+GLuint obj_indices[] = {     // note that we start from 0
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
 };
 
-int main() {
-    Window window(800, 600, "Hello CMake");
+struct Obj {
+    Obj(const void* vertices, const void* indices, const size_t vsize, const size_t isize, const VertexBufferLayout& layout, const GLenum indexType = GL_UNSIGNED_INT):
+    vao(), vbo(vertices, vsize), ebo(indexType, indices, isize) {
+        vao.AddBuffer(vbo, layout);
+    }
 
     VertexArray vao;
-    VertexBuffer vbo(vertices, sizeof(vertices));
+    VertexBuffer vbo;
+    IndexBuffer ebo;
 
-    VertexBufferLayout vbol;
-    vbol.Push<GL_FLOAT>(2);
-    vao.AddBuffer(vbo, vbol);
+    void Unbind() {
+        ebo.Unbind();
+        vbo.Unbind();
+        vao.Unbind();
+    }
+};
 
-    IndexBuffer ebo(GL_UNSIGNED_BYTE, indices, sizeof(indices));
+int main() {
+    Window window(800, 600, "Block Game");
+
+    VertexBufferLayout layout;
+    layout.Push<GL_FLOAT>(2);
+    Obj obj(obj_vertices, obj_indices, sizeof(obj_vertices), sizeof(obj_indices), layout);
     Shader shader({vs_src, fs_src, false });
 
     // Unbind everything
-    vbo.Unbind();
-    ebo.Unbind();
-    vao.Unbind();
+    obj.Unbind();
     shader.Unbind();
     GLClearError();
 
@@ -41,7 +51,7 @@ int main() {
 
     while (!window.ShouldClose()) {
         renderer.Clear();
-        renderer.Draw(vao, ebo, shader);
+        renderer.Draw(obj.vao, obj.ebo, shader);
 
         window.SwapBuffers();
         Window::PollEvents();
