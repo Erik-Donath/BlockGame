3532e239f251d09ebf0d23b1cf1708cfc7a7b8d8
C42xe2 <erikdonath2006@gmail.com>
2024-04-22 20:27:36 +0200

    Rendering Transparent
    Es wird jetzt sowohl res/logo.jpg und res/hello.png gerendert, wobei letzteres transparent ist.

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,16 @@ GLuint obj1_indices[] = {     // note that we start from 0
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
 };
+float obj2_vertices[] = {
+        0.25f,  0.55f, 1.0f, 1.0f,   // top right
+        0.25f, 0.05f, 1.0f, 0.0f,  // bottom right
+        -0.25f, 0.05f, 0.0f, 0.0f, // bottom left
+        -0.25f, 0.55f, 0.0f, 1.0f // top left
+};
+GLuint obj2_indices[] = {     // note that we start from 0
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+};
 
 struct Object {
     Object(const void* vertices, const void* indices, const size_t vsize, const size_t isize, const VertexBufferLayout& layout, const GLenum indexType = GL_UNSIGNED_INT):
@@ -44,28 +54,35 @@ int main() {
     layout.Push<GL_FLOAT>(2); // UV-Coord
 
     // Creating Object
-    Object obj1(obj1_vertices, obj1_indices, sizeof(obj1_vertices), sizeof(obj1_indices), layout);
     Shader shader(RESOURCES_PATH "/shader.glsl");
-    Texture texture(RESOURCES_PATH "/logo.jpg");
+    Object obj1(obj1_vertices, obj1_indices, sizeof(obj1_vertices), sizeof(obj1_indices), layout);
+    Object obj2(obj2_vertices, obj2_indices, sizeof(obj2_vertices), sizeof(obj2_indices), layout);
+    Texture texture1(RESOURCES_PATH "/logo.jpg");
+    Texture texture2(RESOURCES_PATH "/hello.png");
 
     // Unbind everything & Clearing Errors
     obj1.Unbind();
     shader.Unbind();
-    texture.Unbind();
+    texture1.Unbind();
+    texture2.Unbind();
     GLClearError();
 
     // Create Renderer
     Renderer renderer;
     renderer.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-    // Setting Texture to current
-    texture.Bind(0);
-    shader.SetUniform1i("u_Texture", 0);
+    // Binding Texture
+    texture1.Bind(0);
+    texture2.Bind(1);
 
     while (!window.ShouldClose()) {
         renderer.Clear();
 
+        shader.SetUniform1i("u_Texture", 0);
         renderer.Draw(obj1.vao, obj1.ebo, shader);
+
+        shader.SetUniform1i("u_Texture", 1);
+        renderer.Draw(obj2.vao, obj2.ebo, shader);
 
         window.SwapBuffers();
         Window::PollEvents();
