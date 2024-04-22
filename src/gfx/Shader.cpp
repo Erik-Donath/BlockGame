f11f21a14275db950b283f8ca2b3136ef2c36ffc
C42xe2 <erikdonath2006@gmail.com>
2024-04-22 18:15:38 +0200

    Added Shader Sample Support
    Der Shader kann jetzt einen Integer Wert als Uniform setzten. Dies wird bei Texturen benötigt.

--- src/gfx/Shader.cpp
+++ src/gfx/Shader.cpp
@@ -33,6 +33,18 @@ void Shader::Unbind() const {
     GLCall(glUseProgram(0));
 }
 
+void Shader::SetUniform1i(const std::string &name, int v0) {
+    Bind();
+    GLCall(glUniform1i(GetUniformLocation(name), v0));
+    Unbind();
+}
+
+void Shader::SetUniform1f(const std::string &name, float v0) {
+    Bind();
+    GLCall(glUniform1f(GetUniformLocation(name), v0));
+    Unbind();
+}
+
 void Shader::SetUniform4f(const std::string &name, float v0, float v1, float v2, float v3) {
     Bind();
     GLCall(glUniform4f(GetUniformLocation(name), v0 , v1, v2, v3));
--- src/gfx/Shader.h
+++ src/gfx/Shader.h
@@ -23,6 +23,8 @@ public:
     void Bind() const;
     void Unbind() const;
 
+    void SetUniform1i(const std::string& name, int v0);
+    void SetUniform1f(const std::string& name, float v0);
     void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
     inline void Use() const {
         Bind();
