e4ec8d622ba4b4af309ab9162d337852fa69c1b9
C42xe2 <erikdonath2006@gmail.com>
2024-04-22 20:31:57 +0200

    Added Background
    Ein Screenshot von Anno ist jetzt der Hintergrund.

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,16 @@ GLuint obj2_indices[] = {     // note that we start from 0
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
 };
+float obj3_vertices[] = {
+        1.0f,  1.0f, 1.0f, 1.0f,   // top right
+        1.0f, -1.0f, 1.0f, 0.0f,  // bottom right
+        -1.0f, -1.0f, 0.0f, 0.0f, // bottom left
+        -1.0f, 1.0f, 0.0f, 1.0f // top left
+};
+GLuint obj3_indices[] = {     // note that we start from 0
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
+};
 
 struct Object {
     Object(const void* vertices, const void* indices, const size_t vsize, const size_t isize, const VertexBufferLayout& layout, const GLenum indexType = GL_UNSIGNED_INT):
@@ -57,14 +67,19 @@ int main() {
     Shader shader(RESOURCES_PATH "/shader.glsl");
     Object obj1(obj1_vertices, obj1_indices, sizeof(obj1_vertices), sizeof(obj1_indices), layout);
     Object obj2(obj2_vertices, obj2_indices, sizeof(obj2_vertices), sizeof(obj2_indices), layout);
+    Object obj3(obj3_vertices, obj3_indices, sizeof(obj3_vertices), sizeof(obj3_indices), layout);
     Texture texture1(RESOURCES_PATH "/logo.jpg");
     Texture texture2(RESOURCES_PATH "/hello.png");
+    Texture texture3(RESOURCES_PATH "/anno.jpg");
 
     // Unbind everything & Clearing Errors
     obj1.Unbind();
-    shader.Unbind();
+    obj2.Unbind();
+    obj3.Unbind();
     texture1.Unbind();
     texture2.Unbind();
+    texture3.Unbind();
+    shader.Unbind();
     GLClearError();
 
     // Create Renderer
@@ -74,9 +89,13 @@ int main() {
     // Binding Texture
     texture1.Bind(0);
     texture2.Bind(1);
+    texture3.Bind(2);
 
     while (!window.ShouldClose()) {
         renderer.Clear();
+
+        shader.SetUniform1i("u_Texture", 2);
+        renderer.Draw(obj3.vao, obj3.ebo, shader);
 
         shader.SetUniform1i("u_Texture", 0);
         renderer.Draw(obj1.vao, obj1.ebo, shader);
