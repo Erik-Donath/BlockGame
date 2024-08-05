e270ed696f6f1eebf2b4982584dfd6842f5276ac
C42xe2 <erikdonath2006@gmail.com>
2024-08-05 20:34:18 +0200

    Lets start over again

--- include/stb/src/stb.c
+++
@@ -1,5 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#include "../include/stb/stb_image.h"
-
-#define STB_TRUETYPE_IMPLEMENTATION
-#include "../include/stb/stb_truetype.h"
--- res/shaders/fragment-shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-in vec2 vUV;
-out vec4 fColor;
-
-uniform sampler2D u_Texture;
-
-void main() {
-    fColor = texture(u_Texture, vUV);
-}
